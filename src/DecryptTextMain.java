f17cf0f9bd068bc925a431e5fc17b43e131a07ad
magiclud <you@example.com>
2014-03-10 12:12:17 +0100

    odszyfrowanie tekst sformatowany

--- src/DecryptTextMain.java
+++ src/DecryptTextMain.java
@@ -6,32 +6,38 @@ public class DecryptTextMain {
 
 		String wiadomosc = "between the hammer and the anvil.";
 		byte[] kryptogram = RC4wersjaWlasciwa.zakoduj(wiadomosc,
-				RC4wersjaWlasciwa.pobierzKlucz(sciezkaDoKeyStore, aliasHasla, hasloDoKeystora));
+				RC4wersjaWlasciwa.pobierzKlucz(sciezkaDoKeyStore, aliasHasla,
+						hasloDoKeystora));
 		RC4wersjaWlasciwa.zapiszZakodowanaWiadomoscDoPilku(kryptogram);
-		
+
 		String wiadomosc2 = "nobody was hurt in the incident.";
 		byte[] kryptogram2 = RC4wersjaWlasciwa.zakoduj(wiadomosc2,
-				RC4wersjaWlasciwa.pobierzKlucz(sciezkaDoKeyStore, aliasHasla, hasloDoKeystora));
-	
-//		System.out.println(wiadomosc);
-//		System.out.println(new String(kryptogram));
-//		System.out.println(wiadomosc2);
-//		System.out.println(new String(kryptogram2)); 
-	
-	//	System.out.println("Xor kryptogramow:    ");
-		byte[] xorKryptogramow = DecryptText.wykonajXor(kryptogram, kryptogram2);
-	//	DecryptText.wyswietlenie(xorKryptogramow);
+				RC4wersjaWlasciwa.pobierzKlucz(sciezkaDoKeyStore, aliasHasla,
+						hasloDoKeystora));
+
+		// System.out.println(wiadomosc);
+		// System.out.println(new String(kryptogram));
+		// System.out.println(wiadomosc2);
+		// System.out.println(new String(kryptogram2));
+
+		// System.out.println("Xor kryptogramow:    ");
+		byte[] xorKryptogramow = DecryptText
+				.wykonajXor(kryptogram, kryptogram2);
+		// DecryptText.wyswietlenie(xorKryptogramow);
 		System.out.println("");
-		
-		/*tablice najczesciej wystepujacych liter*/
+
+		/* tablice najczesciej wystepujacych liter */
 		byte[] tablicaE = new byte[xorKryptogramow.length];
-		
-		for(int i =0; i<xorKryptogramow.length;i++){
-			tablicaE[i]='e';
+
+		for (int i = 0; i < xorKryptogramow.length; i++) {
+			tablicaE[i] = 'e';
 		}
 		System.out.println("tablicaE: " + new String(tablicaE));
-		
-			byte[] xorXorIWyrazenie2 = DecryptText.wykonajXor(xorKryptogramow, tablicaE );
-			System.out.println("Wartosc xora (xorKryptogramow i tablicyLiterE):    " + new String (xorXorIWyrazenie2) );
-}
+
+		byte[] xorXorIWyrazenie2 = DecryptText.wykonajXor(xorKryptogramow,
+				tablicaE);
+		System.out
+				.println("Wartosc xora (xorKryptogramow i tablicyLiterE):    "
+						+ new String(xorXorIWyrazenie2));
+	}
 }
